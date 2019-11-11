8102b973816f3fe324dbed0d1d749ffd430749bf
viktoriabovtrel <55506905+viktoriabovtrel@users.noreply.github.com>
2019-11-11 23:36:41 +0300

    update

--- src/js/base.js
+++ src/js/base.js
@@ -1,1 +1,31 @@
-console.log('Привет')
+/* для адаптивки
+var slides = document.querySelectorAll('.caption_item');
+var prevBtn = document.getElementById('left-button');
+var nextBtn = document.getElementById('right-button');
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+    if (n < 1) {
+        slideIndex = slides.length;
+    } else if (n > slides.length) {
+        slideIndex = 1;
+    }
+    for(var i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none';
+    }
+    slides[slideIndex - 1].style.display = 'block';
+}
+
+function plusSlides(n){
+    showSlides(slideIndex += n);
+}
+
+prevBtn.onclick = function (){
+    plusSlides(-1);
+}
+
+nextBtn.onclick = function (){
+    plusSlides(1);
+}*/
+
