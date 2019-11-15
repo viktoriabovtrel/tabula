3ba29ac7cf81d4f23acbc4916e8629bfe607c7c1
viktoriabovtrel <55506905+viktoriabovtrel@users.noreply.github.com>
2019-11-16 02:00:17 +0300

    update

--- src/js/base.js
+++ src/js/base.js
@@ -14,7 +14,7 @@ function showSlides(n) {
     for(var i = 0; i < slides.length; i++){
         slides[i].style.display = 'none';
     }
-    slides[slideIndex - 1].style.display = 'block';
+    slides[slideIndex - 1].style.display = 'flex';
 }
 
 function plusSlides(n){
