fdd538867ef742f7f4e6245e5ecf9a75165d3b62
viktoriabovtrel <55506905+viktoriabovtrel@users.noreply.github.com>
2019-11-18 02:52:49 +0300

    fixed navbar + animation

--- src/js/base.js
+++ src/js/base.js
@@ -1,3 +1,24 @@
+$(function () {
+    var fixblock_pos = $('#fixblock').position().top;
+    $(window).scroll(function () {
+
+        if ($(window).scrollTop() > fixblock_pos) {
+            $('#fixblock').css({
+                'position': 'fixed',
+                'top': '0px',
+                'background': 'white',
+                'width': '100%'
+            });
+        } else {
+            $('#fixblock').css({
+                'position': 'static',
+                'background': 'none'
+            });
+
+        }
+    })
+});
+
 /* для адаптивки*/
 var slides = document.querySelectorAll('.caption');
 var prevBtn = document.querySelector('.left-button');
@@ -11,26 +32,25 @@ function showSlides(n) {
     } else if (n > slides.length) {
         slideIndex = 1;
     }
-    for(var i = 0; i < slides.length; i++){
+    for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
     }
     slides[slideIndex - 1].style.display = 'flex';
 }
 
-function plusSlides(n){
+function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-prevBtn.onclick = function (){
+prevBtn.onclick = function () {
     plusSlides(-1);
 }
 
-nextBtn.onclick = function (){
+nextBtn.onclick = function () {
     plusSlides(1);
 }
 
 /*
-
 var captionRight = document.querySelector('.caption-right');
 var captionLeft = document.querySelector('.caption-left');
 
@@ -45,3 +65,9 @@ function typeme() {
     captionRight.toggleAttribute('hidden');
 }
 */
+
+var items = $('nav ul li');
+items.css('opacity', 0);
+for (var i = 0; i < items.length; i++) {
+  $(items[i]).delay(i * 400).animate({ opacity: 1 }, 400);
+}
