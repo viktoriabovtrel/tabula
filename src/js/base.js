b4c166bc371d1030cbac646428d3d71f4b328252
viktoriabovtrel <55506905+viktoriabovtrel@users.noreply.github.com>
2019-11-29 23:39:34 +0300

    +

--- src/js/base.js
+++ src/js/base.js
@@ -1,34 +1,3 @@
-// carousel 
-/*var slides = document.querySelectorAll('.caption');
-var prevBtn = document.querySelector('.left-button');
-var nextBtn = document.querySelector('.right-button');
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function showSlides(n) {
-    if (n < 1) {
-        slideIndex = slides.length;
-    } else if (n > slides.length) {
-        slideIndex = 1;
-    }
-    for (var i = 0; i < slides.length; i++) {
-        slides[i].style.display = 'none';
-    }
-    slides[slideIndex - 1].style.display = 'flex';
-}
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-prevBtn.onclick = function () {
-    plusSlides(-1);
-}
-
-nextBtn.onclick = function () {
-    plusSlides(1);
-}*/
-
 
 // navbar
 $(function () {
@@ -66,7 +35,6 @@ for (var i = 0; i < items.length; i++) {
 }
 
 // accordion
-
 $(document).ready(function () {
     $(".question").on('click', ShowAnswer);
 });
